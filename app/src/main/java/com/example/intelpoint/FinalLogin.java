92560a075770d99372efda337428f0360fd02657
Samit007 <bhattarai905@gmail.com>
2020-01-21 14:49:40 +0545

    Design

---
+++ app/src/main/java/com/example/intelpoint/FinalLogin.java
@@ -0,0 +1,14 @@
+package com.example.intelpoint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinalLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_login);
+    }
+}
